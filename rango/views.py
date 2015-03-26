4cbf3cf4b1dd61b48c0ffb14173ce982d6056463
administrator <administrator@ravens1>
2015-03-26 11:27:22 +0000

    introducing templates and static files

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return render_to_response("index.html")
 
