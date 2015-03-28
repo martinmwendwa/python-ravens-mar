c2b6b69dedf27add66dac0eef868544efbd128a5
administrator <administrator@ravens1>
2015-03-28 05:12:19 +0000

    Introducing Models and Databases

--- new_proj/settings.py
+++ new_proj/settings.py
@@ -59,8 +59,11 @@ WSGI_APPLICATION = 'new_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':"rango_project",
+	'USER': 'rango1',
+	'PASSWORD':'martin2015',
+	'HOST':'localhost'
     }
 }
 
