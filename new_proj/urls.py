bd5abd956c438a9d2fe1541ee210af7acf91ad32
administrator <administrator@ravens1>
2015-04-02 11:23:30 +0000

    working with models templates and views

--- new_proj/urls.py
+++ new_proj/urls.py
@@ -5,4 +5,6 @@ from rango import views
 urlpatterns = patterns('',
  url(r'^$', views.index, name='index'),
  url(r'^admin/', include(admin.site.urls)),
+ url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category')  # New!
 )
+ 
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render_to_response
+from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    return render_to_response("index.html")
+    category_list = Category.objects.order_by("-name")
+    context_dict = {"categories":category_list}
+    return render_to_response("index.html",context_dict )
+
+def category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name'] = category.name
+
+		pages = Page.objects.filter(category=category)
+
+
+		context_dict['pages'] = pages
+
+
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+
+	return render(request, 'rango/category.html', context_dict)
 
